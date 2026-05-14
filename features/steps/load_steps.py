ee84e5f2adcb988b53ebcb5c01ff77d82e046f63
moawinds <alwisaputraa05@gmail.com>
2026-05-14 18:55:08 +0700

    Refactor load_steps.py by removing copyright and cleaning comments
    Removed copyright notice and HTTP status codes. Updated comments for clarity.

--- features/steps/load_steps.py
+++ features/steps/load_steps.py
@@ -1,40 +1,23 @@
-######################################################################
-# Copyright 2016, 2023 John J. Rofrano. All Rights Reserved.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-# https://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-######################################################################
-
-"""
-Product Steps
-
-Steps file for products.feature
-
-For information on Waiting until elements are present in the HTML see:
-    https://selenium-python.readthedocs.io/waits.html
-"""
 import requests
 from behave import given
 
-# HTTP Return Codes
-HTTP_200_OK = 200
-HTTP_201_CREATED = 201
-HTTP_204_NO_CONTENT = 204
-
 @given('the following products')
 def step_impl(context):
-    """ Delete all Products and load new ones """
-    #
-    # List all of the products and delete them one by one
+    """Delete all Products and load new ones"""
+    rest_endpoint = f"{context.BASE_URL}/products"
+    context.resp = requests.get(rest_endpoint)
+    for product in context.resp.json():
+        requests.delete(f"{rest_endpoint}/{product['id']}")
+    
+    for row in context.table:
+        payload = {
+            "name": row['name'],
+            "description": "BDD Test Data",
+            "price": "10.00",
+            "available": row['available'] in ['True', 'true', '1'],
+            "category": row['category']
+        }
+        requests.post(rest_endpoint, json=payload)    # List all of the products and delete them one by one
     #
     rest_endpoint = f"{context.base_url}/products"
     context.resp = requests.get(rest_endpoint)
