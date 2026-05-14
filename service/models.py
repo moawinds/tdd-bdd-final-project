838490f7e3978f7607a92773172c5a5bffd7df59
moawinds <alwisaputraa05@gmail.com>
2026-05-14 19:00:25 +0700

    Revise Product model and category enumeration
    Refactor Product model and update category handling.

--- service/models.py
+++ service/models.py
@@ -1,249 +1,77 @@
-# Copyright 2016, 2023 John Rofrano. All Rights Reserved.
-#
-# Licensed under the Apache License, Version 2.0 (the 'License');
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-# https://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an 'AS IS' BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
-"""
-Models for Product Demo Service
-
-All of the models are stored in this module
-
-Models
-------
-Product - A Product used in the Product Store
-
-Attributes:
------------
-name (string) - the name of the product
-description (string) - the description the product belongs to (i.e., dog, cat)
-available (boolean) - True for products that are available for adoption
-
-"""
 import logging
-from enum import Enum
-from decimal import Decimal
-from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
 logger = logging.getLogger("flask.app")
 
-# Create the SQLAlchemy object to be initialized later in init_db()
+# Inisialisasi SQLAlchemy
 db = SQLAlchemy()
 
-
-def init_db(app):
-    """Initialize the SQLAlchemy app"""
-    Product.init_db(app)
-
-
 class DataValidationError(Exception):
-    """Used for an data validation errors when deserializing"""
+    """Digunakan untuk kesalahan validasi data dari deserialisasi"""
 
-
-class Category(Enum):
-    """Enumeration of valid Product Categories"""
-
-    UNKNOWN = 0
-    CLOTHS = 1
-    FOOD = 2
-    HOUSEWARES = 3
-    AUTOMOTIVE = 4
-    TOOLS = 5
-
+class Category:
+    """Enumerasi Kategori Produk"""
+    UNKNOWN = "UNKNOWN"
+    CLOTHS = "CLOTHS"
+    FOOD = "FOOD"
+    ELECTRONICS = "ELECTRONICS"
+    FURNITURE = "FURNITURE"
 
 class Product(db.Model):
     """
-    Class that represents a Product
-
-    This version uses a relational database for persistence which is hidden
-    from us by SQLAlchemy's object relational mappings (ORM)
+    Kelas yang merepresentasikan sebuah Produk
     """
+    app = None
 
-    ##################################################
-    # Table Schema
-    ##################################################
+    # Tabel Schema
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(100), nullable=False)
-    description = db.Column(db.String(250), nullable=False)
-    price = db.Column(db.Numeric, nullable=False)
+    name = db.Column(db.String(63), nullable=False)
+    description = db.Column(db.String(256))
+    price = db.Column(db.Decimal(10, 2), nullable=False)
     available = db.Column(db.Boolean(), nullable=False, default=True)
-    category = db.Column(
-        db.Enum(Category), nullable=False, server_default=(Category.UNKNOWN.name)
-    )
-
-    ##################################################
-    # INSTANCE METHODS
-    ##################################################
+    category = db.Column(db.String(63), nullable=False, server_default=Category.UNKNOWN)
 
     def __repr__(self):
         return f"<Product {self.name} id=[{self.id}]>"
 
     def create(self):
-        """
-        Creates a Product to the database
-        """
+        """Membuat produk baru ke database"""
         logger.info("Creating %s", self.name)
-        # id must be none to generate next primary key
-        self.id = None  # pylint: disable=invalid-name
+        self.id = None  # id harus None agar database bisa menentukannya
         db.session.add(self)
         db.session.commit()
 
     def update(self):
-        """
-        Updates a Product to the database
-        """
-        logger.info("Saving %s", self.name)
+        """Memperbarui produk yang sudah ada"""
+        logger.info("Updating %s", self.name)
         if not self.id:
             raise DataValidationError("Update called with empty ID field")
         db.session.commit()
 
     def delete(self):
-        """Removes a Product from the data store"""
+        """Menghapus produk dari database"""
         logger.info("Deleting %s", self.name)
         db.session.delete(self)
         db.session.commit()
 
-    def serialize(self) -> dict:
-        """Serializes a Product into a dictionary"""
+    def serialize(self):
+        """Mengubah objek menjadi dictionary untuk JSON"""
         return {
             "id": self.id,
             "name": self.name,
             "description": self.description,
             "price": str(self.price),
             "available": self.available,
-            "category": self.category.name  # convert enum to string
+            "category": self.category
         }
 
-    def deserialize(self, data: dict):
-        """
-        Deserializes a Product from a dictionary
-        Args:
-            data (dict): A dictionary containing the Product data
-        """
+    def deserialize(self, data):
+        """Mengubah dictionary menjadi objek Produk"""
         try:
             self.name = data["name"]
-            self.description = data["description"]
-            self.price = Decimal(data["price"])
-            if isinstance(data["available"], bool):
-                self.available = data["available"]
-            else:
-                raise DataValidationError(
-                    "Invalid type for boolean [available]: "
-                    + str(type(data["available"]))
-                )
-            self.category = getattr(Category, data["category"])  # create enum from string
-        except AttributeError as error:
-            raise DataValidationError("Invalid attribute: " + error.args[0]) from error
+            self.description = data.get("description")
+            self.price = data["price"]
+            self.available = data["available"]
+            self.category = data["category"]
         except KeyError as error:
-            raise DataValidationError("Invalid product: missing " + error.args[0]) from error
-        except TypeError as error:
-            raise DataValidationError(
-                "Invalid product: body of request contained bad or no data " + str(error)
-            ) from error
-        return self
-
-    ##################################################
-    # CLASS METHODS
-    ##################################################
-
-    @classmethod
-    def init_db(cls, app: Flask):
-        """Initializes the database session
-
-        :param app: the Flask app
-        :type data: Flask
-
-        """
-        logger.info("Initializing database")
-        # This is where we initialize SQLAlchemy from the Flask app
-        db.init_app(app)
-        app.app_context().push()
-        db.create_all()  # make our sqlalchemy tables
-
-    @classmethod
-    def all(cls) -> list:
-        """Returns all of the Products in the database"""
-        logger.info("Processing all Products")
-        return cls.query.all()
-
-    @classmethod
-    def find(cls, product_id: int):
-        """Finds a Product by it's ID
-
-        :param product_id: the id of the Product to find
-        :type product_id: int
-
-        :return: an instance with the product_id, or None if not found
-        :rtype: Product
-
-        """
-        logger.info("Processing lookup for id %s ...", product_id)
-        return cls.query.get(product_id)
-
-    @classmethod
-    def find_by_name(cls, name: str) -> list:
-        """Returns all Products with the given name
-
-        :param name: the name of the Products you want to match
-        :type name: str
-
-        :return: a collection of Products with that name
-        :rtype: list
-
-        """
-        logger.info("Processing name query for %s ...", name)
-        return cls.query.filter(cls.name == name)
-
-    @classmethod
-    def find_by_price(cls, price: Decimal) -> list:
-        """Returns all Products with the given price
-
-        :param price: the price to search for
-        :type name: float
-
-        :return: a collection of Products with that price
-        :rtype: list
-
-        """
-        logger.info("Processing price query for %s ...", price)
-        price_value = price
-        if isinstance(price, str):
-            price_value = Decimal(price.strip(' "'))
-        return cls.query.filter(cls.price == price_value)
-
-    @classmethod
-    def find_by_availability(cls, available: bool = True) -> list:
-        """Returns all Products by their availability
-
-        :param available: True for products that are available
-        :type available: str
-
-        :return: a collection of Products that are available
-        :rtype: list
-
-        """
-        logger.info("Processing available query for %s ...", available)
-        return cls.query.filter(cls.available == available)
-
-    @classmethod
-    def find_by_category(cls, category: Category = Category.UNKNOWN) -> list:
-        """Returns all Products by their Category
-
-        :param category: values are ['MALE', 'FEMALE', 'UNKNOWN']
-        :type available: enum
-
-        :return: a collection of Products that are available
-        :rtype: list
-
-        """
-        logger.info("Processing category query for %s ...", category.name)
-        return cls.query.filter(cls.category == category)
+            raise DataValidationError("Invalid Product: missing " + error.args
